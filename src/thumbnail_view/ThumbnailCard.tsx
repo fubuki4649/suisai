956640b97a5195718dd1451d7b0d350b3f8239e8
fubuki4649 <boku@kaneki.dev>
2025-03-08 03:49:51 -0500

    implemented card selection

--- src/thumbnail_view/ThumbnailCard.tsx
+++ src/thumbnail_view/ThumbnailCard.tsx
@@ -1,28 +1,33 @@
 import {Card, CardFooter, Image} from "@heroui/react";
 import React from "react";
 import {ThumbnailCardProps} from "./ViewModel.ts";
+import clsx from "clsx";
 
 
 function ThumbnailCard(props: ThumbnailCardProps) {
 
   const handleClick = () => {
-    console.log("Click");
     props.onClick(props.id);
   };
 
   return (
-    <Card isPressable onPress={handleClick} shadow="sm" className="h-fit w-fit">
+    <Card
+      isPressable
+      onPress={handleClick}
+      shadow={clsx(props.isSelected ? "lg" : "sm") as ("lg" | "sm")}
+      className={clsx(props.isSelected && "border-2 border-secondary", "h-fit w-fit")}
+    >
       <Image
         alt="Card background"
         className="rounded-xl"
         src={props.contentUrl}
         width={270}
       />
-      <CardFooter >
-        <h1>
-          {props.id}
-        </h1>
-      </CardFooter>
+      {/*<CardFooter >*/}
+      {/*  <h1>*/}
+      {/*    {props.id}*/}
+      {/*  </h1>*/}
+      {/*</CardFooter>*/}
     </Card>
   )
 }
--- src/thumbnail_view/ThumbnailContainer.tsx
+++ src/thumbnail_view/ThumbnailContainer.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import ThumbnailCard from "./ThumbnailCard.tsx";
 import {ThumbnailCardProps} from "./ViewModel.ts";
+import ImageDetailCard from "./ImageDetailCard.tsx";
 
 
 function ThumbnailContainer() {
@@ -9,25 +10,33 @@ function ThumbnailContainer() {
 
   const onCardSelect = (cardId: string) => {
     setSelectedCardId(cardId);
+
+    setCards(cards =>
+      cards.map(card => (card.isSelected = (card.id === cardId), card))
+    )
   }
 
-  const cards: ThumbnailCardProps[] = [
-    {id: "juan", contentUrl: "https://heroui.com/images/hero-card-complete.jpeg", isSelected: false, onClick: onCardSelect},
-    {id: "つ", contentUrl: "https://heroui.com/images/hero-card-complete.jpeg", isSelected: false, onClick: onCardSelect},
-    {id: "three", contentUrl: "https://heroui.com/images/hero-card-complete.jpeg", isSelected: false, onClick: onCardSelect}
-  ]
+  const [cards, setCards] = useState<ThumbnailCardProps[]>([
+    {id: "juan", contentUrl: "https://cdn.jetphotos.com/full/5/40931_1587092365.jpg", isSelected: false, onClick: onCardSelect},
+    {id: "つ", contentUrl: "https://cdn.jetphotos.com/full/6/1042605_1741347047.jpg", isSelected: false, onClick: onCardSelect},
+    {id: "three", contentUrl: "https://cdn.jetphotos.com/full/6/92518_1541393083.jpg", isSelected: false, onClick: onCardSelect},
+    {id: "fo", contentUrl: "https://cdn.jetphotos.com/full/5/66513_1538107938.jpg", isSelected: false, onClick: onCardSelect}
+  ]);
+
 
   return (
-    <ul className="flex flex-wrap flex-grow content-start gap-6 p-6 grid-cols-auto">
-        {
-          cards.map((card) => (
-            <li key={card.id}>
-              <ThumbnailCard {...card} />
-            </li>
-          ))
-        }
-        <h1 className="bg-black">Selected Item: {selectedCardID}</h1>
-    </ul>
+    <>
+      <ul className="flex flex-wrap flex-grow content-start gap-6 p-6 grid-cols-auto">
+        {cards.map((card) => (
+          <li key={card.id}>
+            <ThumbnailCard {...card} />
+          </li>
+        ))}
+        <h1 className="bg-background">Selected Item: {selectedCardID}</h1>
+      </ul>
+
+      <ImageDetailCard/>
+    </>
   )
 }
 
