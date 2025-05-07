9de26122a8f9cf0a87e5704953c5c0705f56ee25
fubuki4649 <git@franktao.com>
2025-05-07 02:42:23 -0400

    Added Image Actions Card

---
+++ src/ui/thumbnail_view/ImageActionsCard.tsx
@@ -0,0 +1,37 @@
+import {Button, Card, CardBody, CardHeader, Tooltip} from "@heroui/react";
+import {CheckCircleIcon, NoSymbolIcon, TrashIcon, TruckIcon} from "@heroicons/react/24/outline";
+import React from "react";
+import {useSelectedPhotos} from "../../models/GlobalContext.tsx";
+
+
+export default function ImageActionsCard() {
+
+  const [selectedPhotos] = useSelectedPhotos();
+
+  return (
+    <Card shadow="md" className="h-fit w-80 m-6">
+      <CardHeader className="p-4 w-full flex-col">
+        <div className="flex flex-row justify-between w-full">
+          <h4 className="font-bold text-large">Selected Photos</h4>
+          <p className="text-default-600 opacity-90">({selectedPhotos.length})</p>
+        </div>
+      </CardHeader>
+      <CardBody className="p-4 !pt-0 flex-col">
+        <div className="flex flex-row w-full gap-4 justify-evenly">
+          <Tooltip content="Select All">
+            <Button isIconOnly className="p-1.5" aria-label="Select All" color="secondary" variant="light"><CheckCircleIcon /></Button>
+          </Tooltip>
+          <Tooltip content="Unselect All">
+            <Button isIconOnly className="p-1.5" aria-label="Unselect All" color="secondary" variant="light"><NoSymbolIcon /></Button>
+          </Tooltip>
+          <Tooltip content="Move To Album">
+            <Button isIconOnly className="p-1.5" aria-label="Move To Album" color="success" variant="flat"><TruckIcon /></Button>
+          </Tooltip>
+          <Tooltip content="Delete Images" color="danger">
+            <Button isIconOnly className="p-2" aria-label="Delete" color="danger" variant="shadow"><TrashIcon /></Button>
+          </Tooltip>
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
--- src/ui/thumbnail_view/ImageDetailCardStack.tsx
+++ src/ui/thumbnail_view/ImageDetailCardStack.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import {useSelectedPhotos} from "../../models/GlobalContext.tsx";
 import {DetailCardProps} from "./ViewModel.ts";
 import ImageDetailCard from "./ImageDetailCard.tsx";
+import {Button, Card, CardBody, CardHeader, Tooltip} from "@heroui/react";
+import {CheckCircleIcon, NoSymbolIcon, TrashIcon, TruckIcon} from "@heroicons/react/24/outline";
+import ImageActionsCard from "./ImageActionsCard.tsx";
 
 
 export default function ImageDetailCardStack() {
@@ -9,31 +12,38 @@ export default function ImageDetailCardStack() {
   const [selectedPhotos] = useSelectedPhotos();
 
   return(
-    <div className="relative w-80 m-6">
-      {
-        [...selectedPhotos].reverse().map((photo, index) => {
+    <div className="flex flex-col justify-between">
+      <div className="relative w-80 m-6">
+        {
+          [...selectedPhotos].reverse().map((photo, index) => {
 
-          const offset = index * 20; // vertical stacking offset
-          const scale = 1 - index * 0.02; // slight scale-down effect
+            // Only show the first 4 photos in the stack
+            if (index > 3) return;
 
-          const cardProps: DetailCardProps = {
-            ...photo,
-            photoDate: new Date(photo.photoDate)
-          }
+            const offset = index * 20; // vertical stacking offset
+            const scale = 1 - index * 0.02; // slight scale-down effect
 
-          return (
-            <div className="absolute top-0 left-0 w-full transition-all" key={photo.photoId}
-              style={{
-                transform: `translateY(${offset}px) scale(${scale})`,
-                  zIndex: selectedPhotos.length - index,
-                  opacity: index === 0 ? 1 : 0.8,
-              }}
-            >
-              <ImageDetailCard {...cardProps} />
-            </div>
-          );
+            const cardProps: DetailCardProps = {
+              ...photo,
+              photoDate: new Date(photo.photoDate)
+            }
 
-      })}
+            return (
+              <div className="absolute top-0 left-0 w-full transition-all" key={photo.photoId}
+                   style={{
+                     transform: `translateY(${offset}px) scale(${scale})`,
+                     zIndex: selectedPhotos.length - index,
+                     opacity: index === 0 ? 1 : 0.8,
+                   }}
+              >
+                <ImageDetailCard {...cardProps} />
+              </div>
+            );
+
+          })}
+      </div>
+
+      <ImageActionsCard />
     </div>
   )
 
