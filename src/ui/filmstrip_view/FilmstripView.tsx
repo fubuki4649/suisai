c00fa3d2005f3c67d5951457865163cbffc4fe2c
fubuki4649 <git@franktao.com>
2025-06-21 20:52:55 -0400

    Fix some display issues

--- src/ui/filmstrip_view/FilmstripView.tsx
+++ src/ui/filmstrip_view/FilmstripView.tsx
@@ -4,7 +4,7 @@ import BigScreen from "./BigScreen.tsx";
 
 function FilmstripView() {
   return (
-    <div className="flex flex-col flex-grow">
+    <div className="flex flex-col flex-grow" style={{ zoom: (window.innerHeight <= 800 ? "0.825" : "1.0") }}>
       <BigScreen/>
       <Filmstrip/>
     </div>
