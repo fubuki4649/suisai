694579a551600cb2c7673dab72d61321aa7e75b9
fubuki4649 <git@franktao.com>
2026-08-16 19:49:15 +0800

    icons no longer remotely loaded from CDN

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import {createRoot} from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import {GalleryContextProvider} from "./context/GalleryContext.tsx";
+import {addCollection} from "@iconify/react";
+import {icons as gravityUiIcons} from "@iconify-json/gravity-ui";
+
+// Register Gravity UI icons locally for zero-latency offline static bundling
+addCollection(gravityUiIcons);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
