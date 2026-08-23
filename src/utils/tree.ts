1e3fbf67f13269c339b4d961eea7f893f53ac6ca
fubuki4649 <git@franktao.com>
2026-08-24 02:49:00 +0800

    removed unused code

--- src/utils/tree.ts
+++ src/utils/tree.ts
@@ -1,17 +1,17 @@
 import {Collection} from "../types/models.ts";
-import PriorityQueue from "js-priority-queue";
 
 // Find a collection by its ID in a tree of collections via BFS
 export function findCollectionByID(id: string, collections: Collection[]): Collection | undefined {
-  const pq = new PriorityQueue<Collection>();
+  const queue: Collection[] = [...collections];
 
-  for (const collection of collections) pq.queue(collection);
-
-  while (pq.length > 0) {
-    const collection = pq.dequeue();
+  while (queue.length > 0) {
+    const collection = queue.shift()!;
 
     if (collection.id === id) return collection;
-    for (const child of collection.children) pq.queue(child);
+    if (collection.children && collection.children.length > 0) {
+      queue.push(...collection.children);
+    }
   }
   return undefined;
 }
+
